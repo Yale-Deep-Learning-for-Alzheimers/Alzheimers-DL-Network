4f98713bb1d2aeb90ec426e264a51ad384827c3b
Faiaz Rahman <faiazrahman15@gmail.com>
2020-04-28 16:03:53 -0700

    Update data_loader Dataset to take array of image paths

--- model/data_loader.py
+++ model/data_loader.py
@@ -1,5 +1,5 @@
 import os
-import sys
+import sys # possibly don't need
 import numpy as np
 
 import torch
@@ -12,31 +12,41 @@ import nibabel as nib
 class MRIData(Dataset):
     """
     MRI data
+    The dictionaries AD_Img_Dict.pkl and MCI_Img_Dict.pkl contain key-value
+      pairs of the following:
+      key:      subject ID
+      value:    paths to relevant images (i.e. multiple images per subject)
+    These dictionaries are converted to arrays and passed into this dataset,
+    where the paths will be accessed and their neuroimages processed into tensors.
     """
 
-    def __init__(self, root_dir, data_file):
+    def __init__(self, root_dir, data_array):
         """
         Args:
             root_dir (string): directory of all the images
-            NOTE: Since we have key-[value]* pairs, we'll probably need to modify this to the root of the dictionary
-            data_file (string): file name of the .csv
+            data_array (list): array that contains one [key, [value]*] entry for each patient,
+                               where key:       subject ID
+                                     value:     paths to patient's MRI .nii neuroimages
         """
         self.root_dir = root_dir
-        self.data_file = data_file
+        self.data_array = data_array
 
     def __len__(self):
         """
         Required by DataLoader
         """
-        return sum(1 for line in open(self.data_file)) # the number of entries in the .csv
-        """ TODO: are all the subjects in the CSVs' rows unique? I think it looked like it """
+        return len(self.data_array) # the number of patients in the dataset
 
     def __getitem__(self, index):
         """
         Allows indexing of dataset; required by DataLoader
+        Returns a tensor that contains the patient's MRI neuroimages and their diagnoses (AD or MCI)
         """
-        pass
-    """ TODO: use nib to get the file and load it; should we convert to tensor here and return the tensor? """
+        # Get current_patient, where [0] is their ID and [1] is their list of images
+        current_patient = self.data_array[index]
+        # For each image path, process the .nii image using nibabel
+        for image_path in current_patient[1]:
+            pass
 
 
 class DataLoader(object):
